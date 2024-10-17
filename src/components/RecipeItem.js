a9e8366a54279e929b9c84723402c16a5a0ff27f
Lina Urrego <urrego.linam@gmail.com>
2024-10-16 23:52:53 -0400

    added items needed for. recipeitem

--- src/components/RecipeItem.js
+++ src/components/RecipeItem.js
@@ -1,11 +1,17 @@
 import React from 'react';
 
-const RecipeItem = ({recipe, deleteRecipe}) =>{
+const RecipeItem = ({
+    recipe,
+    deleteRecipe,
+    editRecipeClick,
+    index,
+}) =>{
     return(
         <div className="recipe">
             <h2>{recipe.name}</h2>
             <p>{recipe.description}</p>
             <button onClick ={() => deleteRecipe(recipe.id)}>Delete</button>
+            <button onClick ={() => editRecipeClick(index)}>Edit</button>
         </div>
     );
 };
