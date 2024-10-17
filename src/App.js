d7404af9e6ddf9324e19400cd2e7bf97c1d3dc16
Lina Urrego <urrego.linam@gmail.com>
2024-10-16 23:51:24 -0400

    added edit and save function in app.js

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import RecipeList from './components/RecipeList';
 import AddRecipe from './components/AddRecipe';
+import EditRecipe from './components/EditRecipe';
 import './App.css';
 
 // Functional component called App
 function App() {
   // State for recipes
   const [recipes, setRecipes] = useState([]);
+  const [editIndex, setEditIndex] = useState(null);
 
   // Function to add a new recipe
   const addRecipe = (name, description) => {
@@ -26,11 +28,50 @@ function App() {
     setRecipes(recipes.filter(recipe => recipe.id !== id)); // Remove recipe with matching ID
   };
 
+// Function editing a recipe by index, changing the name and description
+const editRecipeClick = (index) => {
+  setEditIndex(index);
+
+};
+
+const handleSaveRecipeEdit = (index, name, description) => {
+  const newRecipes = recipes.map((recipe, i) => {
+    if (i === index) {
+      return { id: recipe.id, name, description };
+    }
+    return recipe;
+  });
+  setRecipes(newRecipes);
+  setEditIndex(null); // Reset edit index
+};
+
+
   return (
     <div className="App">
       <Header /> 
       <AddRecipe addRecipe={addRecipe} /> 
-      <RecipeList recipes={recipes} deleteRecipe={deleteRecipe} /> 
+
+ {/* //shows recipe editing form if editIndex is not null,  otherwise not shown. */}
+      {editIndex !== null ?( 
+      <EditRecipe 
+        index={editIndex}
+        setEditIndex={setEditIndex}
+        recipes={recipes}
+        setRecipes={setRecipes}/>
+
+      ):(
+
+      <RecipeList 
+        recipes={recipes}
+        deleteRecipe={deleteRecipe}
+        editRecipeClick={editRecipeClick}  //passing the edit function
+        handleSaveRecipeEdit={handleSaveRecipeEdit} // passing the save function
+        editIndex={editIndex} // passing the edit index
+      /> 
+      )}
+        {/* passing footer because it is not used  */}
+      <Footer />  
+
     </div>
   );
 }
